46a2dfb2ea8ba717b0b1b28288cabd19b3d60a54
Pepper Allaquo <pepperal@ti-l4.skd.21-school.uz>
2024-11-23 21:33:20 +0500

    array

---
+++ array10.c
@@ -0,0 +1,32 @@
+// Eng katta 2chi element
+
+#include <stdio.h>
+
+int main(){
+    int arr[] = {1,2,6,4,7,5};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+        for (int k = 0; k < len - 1; k++)
+        {
+            int temp = 0;
+            if(arr[i] > arr[k]){
+                temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+            }
+        }
+        
+    }
+    
+    // for (int i = 0; i < len; i++)
+    // {
+    //     if (arr[i] > arr[i+1])
+    //     {
+    //         printf("%d",arr[i+1]);
+    //     }
+    // }
+    printf("%d",arr[1]);
+    return 0;
+}
